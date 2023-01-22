b91dd24cc883a3f8de108bcab5442b5d3716f3c8
Liam-McLeod <liam.mcleod42@gmail.com>
2023-01-21 20:48:18 -0500

    Reworked Get Data

--- Plot.py
+++ Plot.py
@@ -15,89 +15,87 @@ def plotData(df,stat_input,format,x_axis):
         new_df = df[[x_axis,stat_input]]
         st.write(new_df)
 
-def compareData():
-    player_input_list = player_input.split(",")
-    if option1 == 'Player':
-        player_dict = players.get_players()
-        
-
 def getData():
+    input_list = user_input.split(",")
     
-    if comparing:
-        return compareData()
-
     if option1 == 'Player':
-        
-        # LOAD ALL PLAYERS INTO DICT
         player_dict = players.get_players()
 
-        # FIND PLAYERS THAT MATCH INPUT
-        player_list = [player for player in player_dict if player['full_name'].lower() == player_input.lower()]
+        player_list = []
+        for p in input_list:
+            for player in player_dict:
+                if player['full_name'].lower() == p.lower():
+                    player_list.append(player)
 
-        # CHECK IF PLAYER FOUND
+        # CHECK IF PLAYERS FOUND
         if not player_list:
             # NO PLAYER FOUND IN LIST
-            st.error('No Player Found', icon="🚨")
+            st.error('No Player(s) Found', icon="🚨")
             return
-            
-        # GET PLAYER AND PLAYER_ID
-        player = player_list[0]
-        player_id = player['id'] 
+        
+        dataframes = []
+        for p in player_list:
+            # PLAYER ID
+            player_id = p['id']
+            # API CALL GETTING PLAYER STATS FROM PLAYER ID
+            data = playercareerstats.PlayerCareerStats(player_id, per_mode36='PerGame')
+            if option2 == 'Regular Season':
+                # REGULAR SEASON STATS
+                df = data.get_data_frames()[0]
+            elif option2 == 'Post Season':
+                # POST SEASON STATS
+                df = data.get_data_frames()[2]
+            dataframes.append(df)
 
-        # API CALL GETTING PLAYER STATS FROM PLAYER_ID
-        data = playercareerstats.PlayerCareerStats(player_id, per_mode36='PerGame')
 
-        if option2 == 'Regular Season': 
-
-            # REGULAR SEASON DATA
-            df = data.get_data_frames()[0]
+        final_df = pd.DataFrame()
+        for df in dataframes:
+            #final_df.insert(0,df.iat[0,0],df[stat_input])
             plotData(df,stat_input,format,x_axis="SEASON_ID")
-
-        elif option2 == "Post Season":
-
-            # POST SEASON DATA
-            df = data.get_data_frames()[2]
-            plotData(df,stat_input,format,x_axis="SEASON_ID")
+        #st.write(final_df)
 
     elif option1 == "Team":
-
         # LOAD ALL TEAMS INTO DICT
         team_dict = teams.get_teams()
 
-        # FIND TEAMS THAT MATCH INPUT
-        team_list = [team for team in team_dict if team['full_name'].lower() == player_input.lower()]
+        team_list = []
+        for t in input_list:
+            for team in team_dict:
+                if team['full_name'].lower() == t.lower():
+                    team_list.append(team)
 
         # CHECK IF TEAM FOUND
         if not team_list:
             # NO TEAM FOUND IN LIST
-            st.error('No Team Found', icon="🚨")
+            st.error('No Team(s) Found', icon="🚨")
             return
 
-        # GET TEAM AND TEAM_ID
-        team = team_list[0]
-        team_id = team['id']
+        dataframes = []
+        for t in team_list:
+            # TEAM ID
+            team_id = t['id']
+            # API CALL GETTING TEAM STATS FROM TEAM ID
+            data = teamyearbyyearstats.TeamYearByYearStats(team_id, per_mode_simple='PerGame')
+            if option2 == 'Regular Season':
+                # REGULAR SEASON STATS
+                df = data.get_data_frames()[0]
+            elif option2 == 'Post Season':
+                # POST SEASON STATS
+                st.error('No Post Season stats for teams', icon="🚨")
+                return
+            dataframes.append(df)
 
-        # API CALL GETTING TEAM STATS FROM TEAM_ID
-        data = teamyearbyyearstats.TeamYearByYearStats(team_id, per_mode_simple='PerGame')
-
-        if option2 == "Regular Season":
-
-            # REGULAR SEASON DATA
-            df = data.get_data_frames()[0]
+        final_df = pd.DataFrame()
+        for df in dataframes:
+            #final_df.insert(0,df.iat[0,0],df[stat_input])
             plotData(df,stat_input,format,x_axis="YEAR")
-            
-        elif option2 == "Post Season":
-            pass
-
+        #st.write(final_df)
 
 # FORUM / WEB PAGE
 
 st.write(""" # NBA Stats""")
 
-comparing = st.checkbox('Compare')
-
-if comparing:
-    st.info('Seperate Two Players or Teams with a Comma', icon="ℹ️")
+st.info('Seperate Up to four players, OR two teams with a comma', icon="ℹ️")
 
 option1 = st.selectbox('Label',('Player','Team'),label_visibility="collapsed")
 
@@ -107,6 +105,6 @@ stat_input = st.selectbox('Label',('PTS', 'REB', 'AST', 'STL','BLK'),label_visib
 
 format = st.selectbox('Label',('Line Graph','Bar Graph','Table'),label_visibility="collapsed")
 
-player_input = st.text_input('Label',label_visibility='collapsed')
+user_input = st.text_input('Label',label_visibility='collapsed')
 
 button = st.button('Add',on_click=getData)
