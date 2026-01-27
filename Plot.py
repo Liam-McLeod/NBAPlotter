b9ca0c5a6a3716b02f5d5a9cfe16cac4c99c57ab
Liam-McLeod <liam.mcleod42@gmail.com>
2026-01-26 20:19:39 -0500

    Added Table Format

--- Plot.py
+++ Plot.py
@@ -1,19 +1,16 @@
-from statistics import mode
 import streamlit as st
 import pandas as pd
 import plotly.express as px
 from nba_api.stats.static import players
 from nba_api.stats.endpoints import playercareerstats
 
-def getPlayerList(dict,input_list):
-    list = []
-    for input in input_list:
-        # REMOVE WHITESPACE
-        input = input.strip()
-        for name in dict:
-            if name['full_name'].lower() == input.lower():
-                list.append(name)
-    return list
+def getPlayerList(player_lookup,player_input_list):
+    results = []
+    for raw_name in player_input_list:
+        name = raw_name.strip().lower()
+        if name in player_lookup:
+            results.append(player_lookup[name])
+    return results
 
 def plotData(combined_df,stat_input,format,x_axis):
 
@@ -22,11 +19,10 @@ def plotData(combined_df,stat_input,format,x_axis):
     elif format == "Bar Graph":
         fig = px.bar(combined_df, x=x_axis, y=stat_input, color='player', barmode='group')
 
-    st.plotly_chart(fig)
-
-    # TODO TABLE FORMAT
-    # elif format == "Table":
-    #     st.write(combined_df[stat_input])
+    #st.plotly_chart(fig)
+    elif format == "Table":
+        table_df = combined_df.pivot_table(index=x_axis, columns='player', values=stat_input).reset_index()
+        st.dataframe(table_df,width='stretch')
 
 def getPlayerStats(player_list,season_type):
     combined = []
@@ -38,39 +34,57 @@ def getPlayerStats(player_list,season_type):
         if stat_type == "Averages":
             playerData = playercareerstats.PlayerCareerStats(player_id, per_mode36='PerGame')
             stats_df = playerData.get_data_frames()[0] if season_type == 'Regular Season' else playerData.get_data_frames()[2]
+
         elif stat_type == "Totals":
             playerData = playercareerstats.PlayerCareerStats(player_id, per_mode36='Totals')
             stats_df = playerData.get_data_frames()[0] if season_type == 'Regular Season' else playerData.get_data_frames()[2]
 
-        # TODO CUMULATIVE TOTALS
-        # elif stat_type == "Totals (Cumulative)":
+        elif stat_type == "Totals (Cumulative)":
+            playerData = playercareerstats.PlayerCareerStats(player_id, per_mode36='Totals')
+            stats_df = playerData.get_data_frames()[0] if season_type == 'Regular Season' else playerData.get_data_frames()[2]
+            stats_df[stat_input] = stats_df[stat_input].cumsum()
 
         stats_df = stats_df.copy()
+        
+        # KEEP ONE ROW PER SEASON, USE TOT IF IT EXISTS. THIS HAPPENS WHEN A PLAYER PLAYS FOR MULTIPLE TEAMS IN A SEASON (i.E. TRADES)
+        if "TEAM_ABBREVIATION" in stats_df.columns:
+            stats_df = (stats_df.sort_values("TEAM_ABBREVIATION").drop_duplicates(subset=["SEASON_ID"], keep="last"))
+
+        # ADD PLAYER NAME COLUMN FOR PLOTTING PURPOSES AND CONVERT SEASON_ID TO INT
         stats_df["player"] = player_name
         stats_df[x_axis] = stats_df[x_axis].apply(lambda x: int(x[:4]) + 1) # Convert '1996-97' to 1997
 
         combined.append(stats_df)
-    combined_df = pd.concat(combined, ignore_index=True)
 
+    # COMBINE ALL PLAYER DATAFRAMES  
+    combined_df = pd.concat(combined, ignore_index=True)
+    # SORT BY X AXIS
+    combined_df = combined_df.sort_values(by=[x_axis])
     return combined_df
 
 def getData():
-    player_input_list = user_input.split(",")
     MAX_INPUT = 4
-    player_dict = players.get_players()
-    player_list = getPlayerList(player_dict,player_input_list)
+    # USER INPUT
+    player_input_list = [x.strip() for x in user_input.split(",") if x.strip()]
+    # LOAD ALL PLAYERS
+    player_list = players.get_players()
+
+    # CREATE PLAYER LOOKUP DICTIONARY FOR FASTER SEARCHING
+    player_lookup = {p['full_name'].lower(): p for p in player_list} # {'lebron james': {player_data}, ...}
+    selected_players = getPlayerList(player_lookup,player_input_list)
 
     # NO PLAYERS FOUND ERROR
-    if not player_list:
+    if not selected_players:
         st.error('No Player(s) Found', icon="🚨")
         return
         
     # TOO MANY PLAYERS ERROR
-    if len(player_list) > MAX_INPUT:
+    if len(selected_players) > MAX_INPUT:
         st.error('Too Many Players to Compare', icon="🚨")
         return
-
-    combined_df = getPlayerStats(player_list,season_type)
+        
+    # GET PLAYER STATS
+    combined_df = getPlayerStats(selected_players,season_type)
 
     # NO DATA FOUND ERROR (E.G. Bill Russell BLK)
     if combined_df[stat_input].isnull().all():
@@ -89,10 +103,10 @@ stat_type = st.selectbox('Label',('Averages','Totals','Totals (Cumulative)'), la
 
 season_type = st.selectbox('Label',('Regular Season','Post Season'),label_visibility="collapsed")
 
-stat_input = st.selectbox('Label',('PTS', 'REB', 'AST', 'STL','BLK'),label_visibility="collapsed")
+stat_input = st.selectbox('Label',('PTS', 'REB', 'AST', 'STL','BLK','FGM','FTM'),label_visibility="collapsed")
 
 format = st.selectbox('Label',('Line Graph','Bar Graph','Table'),label_visibility="collapsed")
 
 user_input = st.text_input('Label',label_visibility='collapsed')
 
-button = st.button('Compare',on_click=getData)
+button = st.button('Compare',width='stretch',on_click=getData)
